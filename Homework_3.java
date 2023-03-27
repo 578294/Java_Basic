4e7ae8866a816680c55a3efe5eb9630691289731
578294 <122666299+578294@users.noreply.github.com>
2023-03-28 02:22:36 +0300

    Homework3

--- Homework_3.java
+++ Homework_3.java
@@ -1,49 +1,51 @@
 import java.util.Scanner;
 public class Homework3 {
-	public static void main(String[] args) {
-		// Переменные для хранения количества правильных и неправильных ответов
-		int correctCount = 0, wrongCount = 0;
+    public static void main(String[] args) {
+        // Переменные для хранения количества правильных и неправильных ответов
+        int correctCount = 0, wrongCount = 0;
 
-		// Массив вопросов
-		String[] questions = {"Назовите кратное число",
-				"Назовите столицу Франции",
-				"Назовите столицу Испании"};
+        // Массив вопросов
+        String[] questions = {"Назовите кратное число",
+                              "Назовите столицу Франции",
+                              "Назовите столицу Испании"};
 
-		// Массив вариантов ответов
-		String[][] answerOptions = {
-				{"1. 14", "2. 15", "3. 13"},
-				{"1. Берлин", "2. Париж", "3. Мадрид"},
-				{"1. Берлин", "2. Париж", "3. Мадрид"}
-		};
+        // Массив вариантов ответов
+        String[][] answerOptions = new String [3][];
+        answerOptions[0] = new String[]{"1. 14", "2. 15", "3. 13", "4. 11"};
+        answerOptions[1] = new String[]{"1. Берлин", "2. Париж", "3. Мадрид"};
+        answerOptions[2] = new String[]{"1. Берлин", "2. Париж", "3. Мадрид", "4. Барселона"};
 
-		// Массив правильных ответов
-		int[] correctAnswers = {1, 2, 3}; //задаю варианты ответов
+        // Массив правильных ответов
+        int[] correctAnswers = new int[3]; //задаю варианты ответов
+        correctAnswers[0] = 1;
+        correctAnswers[1] = 2;
+        correctAnswers[2] = 3;
 
-		Scanner scanner = new Scanner(System.in);
-		// Цикл по всем вопросам
-		for (int i = 0; i < 3; i++) {
-			System.out.println(questions[i]);
-			for (int j = 0; j < 2; j++) {
-				System.out.println(answerOptions[i][j]);
-			}
-			System.out.println("Ваш ответ: ");
-			// Считываем с консоли ответ пользователя
-			int answer = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        // Цикл по всем вопросам
+        for (int i = 0; i < questions.length; i++) {
+            System.out.println(questions[i]);
+            for (int j = 0; j < answerOptions[i].length; j++) {
+                System.out.println(answerOptions[i][j]);
+            }
+            System.out.println("Ваш ответ: ");
+            // Считываем с консоли ответ пользователя
+            int answer = scanner.nextInt();
 
-			// Проверяем и выводим результат
-			if (answer == correctAnswers[i]) {
-				System.out.println(answer);
-				System.out.println("Верно!");
-				correctCount = correctCount + 1;
-			} else {
-				System.out.println(answer);
-				System.out.println("Не верно");
-				wrongCount = wrongCount + 1;
-			}
-			System.out.println();
-		}
+            // Проверяем и выводим результат
+            if (answer == correctAnswers[i]) {
+                System.out.println(answer);
+                System.out.println("Верно!");
+                correctCount = correctCount + 1;
+            } else {
+                System.out.println(answer);
+                System.out.println("Не верно");
+                wrongCount = wrongCount + 1;
+            }
+            System.out.println();
+        }
 
-		//Выводим общий результат
-		System.out.println("Результат: правильно " + correctCount + ", неправильно " + wrongCount);
-	}
+        //Выводим общий результат
+        System.out.println("Результат: правильно " + correctCount + ", неправильно " + wrongCount);
+    }
 }
